a0f674d71aff7cad382d7dc4efc4168af6576346
Yakir <yakir@bmby.com>
2022-01-02 11:58:23 +0200

    ...

--- app/src/main/java/com/example/navagationapp/MainActivity.java
+++ app/src/main/java/com/example/navagationapp/MainActivity.java
@@ -9,15 +9,20 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.DocumentsContract;
+import android.provider.OpenableColumns;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -43,6 +48,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.URISyntaxException;
 import java.util.Iterator;
 import java.util.Objects;
 
@@ -94,11 +100,12 @@ public class MainActivity extends AppCompatActivity {
         Log.d("storagePermissionOk", "getting storage permission checked");
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
             String [] permission = {Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.MANAGE_EXTERNAL_STORAGE};
-            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+            if(ContextCompat.checkSelfPermission(MainActivity.this.getApplicationContext(),
                     READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
-                if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                if(ContextCompat.checkSelfPermission(MainActivity.this.getApplicationContext(),
                         MANAGE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                     // open file picker
+
                     Intent chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
                     // Define file types to xls and xlsx
                     String[] mimeTypes = {"application/vnd.ms-excel","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"};
@@ -116,23 +123,24 @@ public class MainActivity extends AppCompatActivity {
                         }
                         chooseFile.setType(mimeTypesStr.substring(0,mimeTypesStr.length() - 1));
                     }
+
                     startActivityForResult(
                             Intent.createChooser(chooseFile, "Choose a file"),
                             PICKFILE_RESULT_CODE
                     );
                 }else{
-                    ActivityCompat.requestPermissions(this,permission,STORAGE_PERMISSION_REQUEST_CODE);
+                    ActivityCompat.requestPermissions(MainActivity.this,permission,STORAGE_PERMISSION_REQUEST_CODE);
                 }
 
             }else{
-                ActivityCompat.requestPermissions(this,permission,STORAGE_PERMISSION_REQUEST_CODE);
+                ActivityCompat.requestPermissions(MainActivity.this,permission,STORAGE_PERMISSION_REQUEST_CODE);
             }
         }else{
             String [] permission = {Manifest.permission.READ_EXTERNAL_STORAGE};
-            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+            if(ContextCompat.checkSelfPermission(MainActivity.this.getApplicationContext(),
                     READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                 // open file picker
-                Intent chooseFile = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                Intent chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
                 // Define file types to xls and xlsx
                 String[] mimeTypes = {"application/vnd.ms-excel","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"};
                // Ask specifically for something that can be opened:
@@ -172,7 +180,7 @@ public class MainActivity extends AppCompatActivity {
         }
         else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
              // error that we can use google dialog to fix
-            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(this,available,ERROR_DIALOG_REQUEST);
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this,available,ERROR_DIALOG_REQUEST);
             if (dialog != null) {
                 dialog.show();
             }
@@ -183,20 +191,42 @@ public class MainActivity extends AppCompatActivity {
         return false;
 
     }
+    @SuppressLint("Range")
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
+    protected void onActivityResult(int requestCode, int resultCode, Intent result)  {
+        String displayName="empty";
         if(resultCode == RESULT_OK) {
             if (requestCode == PICKFILE_RESULT_CODE ) {
-                if (data != null){
+                if (result != null){
                 Log.d(TAG, "onActivityResult: Proceed to get data from result");
-                Uri uri = data.getData();
+                    Uri uri = result.getData();
+                    String uriString = uri.toString();
+                    File myFile = new File(uriString);
+                    String path = myFile.getAbsolutePath();
+                    Log.d(TAG, "onActivityResult: path is:"+path);
+                    if (uriString.startsWith("content://")) {
+                        Cursor cursor = null;
+                        try {
+                            cursor = MainActivity.this.getContentResolver().query(uri, null, null, null, null);
+                            if (cursor != null && cursor.moveToFirst()) {
+                                Log.d(TAG, "onActivityResult: cursor place, displayName is:"+displayName);
+                                displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                                Log.d(TAG, "onActivityResult: display name is now : "+displayName);
+                            }
+                        } finally {
+                            cursor.close();
+                        }
+                    } else if (uriString.startsWith("file://")) {
+                        displayName = myFile.getName();
+                    }
+                    Log.d(TAG, "onActivityResult: displayName is: "+displayName);
+                    String realFileName = displayName;
 
                 try {
-                    File file = new File(uri.getPath());
-                    Log.d(TAG, "onActivityResult: file getPath is: " + file.getPath());
-                    if (file.exists()) {
-                    FileInputStream fileInputStream = new FileInputStream(file);
+                    Log.d(TAG, "onActivityResult: path is:"+path);
+                    File file = new File(MainActivity.this.getExternalFilesDir(null),realFileName);
+                    FileInputStream fileInputStream = null;
+                    fileInputStream = new FileInputStream(file);
                     workbook = new HSSFWorkbook(fileInputStream);
                     sheet = workbook.getSheetAt(0);
                     for (Row row : sheet) {
@@ -220,15 +250,11 @@ public class MainActivity extends AppCompatActivity {
                         }
 
                     }
-                }else{
-                        Log.d(TAG, "onActivityResult: didn't find file in path: "+uri.getPath());
-                        Toast.makeText(MainActivity.this,"Couldn't access file",Toast.LENGTH_SHORT).show();
-                    }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
 
-            }else{
+                }else{
                     Log.d(TAG, "onActivityResult: fail picking/upload error");
                     Toast.makeText(MainActivity.this,"Error on picking file, try again later",Toast.LENGTH_SHORT)
                             .show();
@@ -239,7 +265,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        super.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, result);
     }
 
 
