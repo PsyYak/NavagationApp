b09535d3827ae4648aaf9134a319731c057df34d
Yakir <yakir@bmby.com>
2022-01-02 16:08:00 +0200

    Added xlsx/xls file import

--- app/src/main/java/com/example/navagationapp/MainActivity.java
+++ app/src/main/java/com/example/navagationapp/MainActivity.java
@@ -1,54 +1,45 @@
 package com.example.navagationapp;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.app.Dialog;
-import android.content.ActivityNotFoundException;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.DocumentsContract;
 import android.provider.OpenableColumns;
-import android.provider.Settings;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 
+
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.BufferedReader;
+
 import java.io.File;
-import java.io.FileDescriptor;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.net.URISyntaxException;
 import java.util.Iterator;
 import java.util.Objects;
 
@@ -194,69 +185,42 @@ public class MainActivity extends AppCompatActivity {
     @SuppressLint("Range")
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent result)  {
-        String displayName="empty";
+
         if(resultCode == RESULT_OK) {
             if (requestCode == PICKFILE_RESULT_CODE ) {
                 if (result != null){
-                Log.d(TAG, "onActivityResult: Proceed to get data from result");
+                 //   Log.d(TAG, "onActivityResult: Proceed to get data from result");
                     Uri uri = result.getData();
-                    String uriString = uri.toString();
-                    File myFile = new File(uriString);
-                    String path = myFile.getAbsolutePath();
-                    Log.d(TAG, "onActivityResult: path is:"+path);
-                    if (uriString.startsWith("content://")) {
-                        Cursor cursor = null;
-                        try {
-                            cursor = MainActivity.this.getContentResolver().query(uri, null, null, null, null);
-                            if (cursor != null && cursor.moveToFirst()) {
-                                Log.d(TAG, "onActivityResult: cursor place, displayName is:"+displayName);
-                                displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
-                                Log.d(TAG, "onActivityResult: display name is now : "+displayName);
-                            }
-                        } finally {
-                            cursor.close();
-                        }
-                    } else if (uriString.startsWith("file://")) {
-                        displayName = myFile.getName();
-                    }
-                    Log.d(TAG, "onActivityResult: displayName is: "+displayName);
-                    String realFileName = displayName;
-
+                    String fileExtension = getFileExt(getApplicationContext(),uri);
+                  //  Log.d(TAG, "onActivityResult: fileExtension:"+fileExtension);
+                    String realFileName = createCopyAndReturnRealPath(getApplicationContext(),uri,"test");
+                    File myFile = new File(Objects.requireNonNull(realFileName));
+                   // Log.d(TAG, "onActivityResult: realFileName path is: "+realFileName);
                 try {
-                    Log.d(TAG, "onActivityResult: path is:"+path);
-                    File file = new File(MainActivity.this.getExternalFilesDir(null),realFileName);
-                    FileInputStream fileInputStream = null;
-                    fileInputStream = new FileInputStream(file);
-                    workbook = new HSSFWorkbook(fileInputStream);
-                    sheet = workbook.getSheetAt(0);
-                    for (Row row : sheet) {
-                        Iterator<Cell> cellIterator = row.cellIterator();
-                        while (cellIterator.hasNext()) {
-                            Cell cell = cellIterator.next();
-
-                            // Check cell type and format accordingly
-                            switch (cell.getCellType()) {
-                                case Cell.CELL_TYPE_NUMERIC:
-                                    // Print cell value
-                                    System.out.println(cell.getNumericCellValue());
-                                    break;
-
-                                case Cell.CELL_TYPE_STRING:
-                                    System.out.println(cell.getStringCellValue());
-                                    break;
-                            }
-
-
+                 //   Log.d(TAG, "onActivityResult: myFile.getName() is:"+myFile.getName());
+                    File file = new File(getFilesDir(),myFile.getName());
+                    FileInputStream fileInputStream =  new FileInputStream(file);
+                    if (fileExtension != null) {
+                        if(fileExtension.contains("xls")){
+                            Log.d(TAG, "onActivityResult: Got xls.");
+                            xlsFilePrint(fileInputStream);
+                        }else if(fileExtension.contains("xlsx")){
+                            Log.d(TAG, "onActivityResult: Got xlsx.");
+                            xlsxFilePrint(fileInputStream);
+                        }else{
+                            Log.d(TAG, "onActivityResult: Not supported file type, notify user.");
+                            Toast.makeText(MainActivity.this,"File extension is not supported, please use either Xlsx or Xls.",Toast.LENGTH_LONG)
+                                    .show();
                         }
-
                     }
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
 
                 }else{
-                    Log.d(TAG, "onActivityResult: fail picking/upload error");
-                    Toast.makeText(MainActivity.this,"Error on picking file, try again later",Toast.LENGTH_SHORT)
+                    Log.d(TAG, "onActivityResult: fail picking/upload error.");
+                    Toast.makeText(MainActivity.this,"Error on picking file, try again later.",Toast.LENGTH_SHORT)
                             .show();
                 }
             }
@@ -268,6 +232,103 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, result);
     }
 
+    @SuppressLint("Range")
+    private String getFileExt(Context context,Uri uri) {
+        String uriString = uri.toString();
+        File myFile = new File(uriString);
+        String path = myFile.getAbsolutePath();
+        String displayName = null;
+
+        if (uriString.startsWith("content://")) {
+            try (Cursor cursor = context.getContentResolver().query(uri, null, null, null, null)) {
+                if (cursor != null && cursor.moveToFirst()) {
+                    displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                    return displayName;
+                }
+            }
+        } else if (uriString.startsWith("file://")) {
+            displayName = myFile.getName();
+            return displayName;
+        }
+        return null;
+    }
+
+    @Nullable
+    public static String createCopyAndReturnRealPath(@NonNull Context context, @NonNull Uri uri,String fileName) {
+        final ContentResolver contentResolver = context.getContentResolver();
+        if (contentResolver == null)
+            return null;
+
+        // Create file path inside app's data dir
+        String filePath = context.getApplicationInfo().dataDir + File.separator +"files"+File.separator+fileName;
+        File file = new File(filePath);
+        try {
+            InputStream inputStream = contentResolver.openInputStream(uri);
+            if (inputStream == null)
+                return null;
+            OutputStream outputStream = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = inputStream.read(buf)) > 0)
+                outputStream.write(buf, 0, len);
+            outputStream.close();
+            inputStream.close();
+        } catch (IOException ignore) {
+            return null;
+        }
+        return file.getAbsolutePath();
+    }
+
+    private void xlsFilePrint(FileInputStream fileInputStream) throws IOException {
+        workbook = new HSSFWorkbook(fileInputStream);
+        sheet = workbook.getSheetAt(0);
+        for (Row row : sheet) {
+            Iterator<Cell> cellIterator = row.cellIterator();
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                // Check cell type and format accordingly
+                switch (cell.getCellType()) {
+                    case Cell.CELL_TYPE_NUMERIC:
+                        // Print cell value
+                        System.out.println(cell.getNumericCellValue());
+                        break;
+
+                    case Cell.CELL_TYPE_STRING:
+                        System.out.println(cell.getStringCellValue());
+                        break;
+                }
+                System.out.println("\t");
+
+            }
+
+        }
+    }
+
+    private void xlsxFilePrint(FileInputStream fileInputStream) throws IOException {
+
+        XSSFWorkbook myWorkBook = new XSSFWorkbook(fileInputStream);
+        sheet = myWorkBook.getSheetAt(0);
+        for (Row row : sheet) {
+            Iterator<Cell> cellIterator = row.cellIterator();
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                // Check cell type and format accordingly
+                switch (cell.getCellType()) {
+                    case Cell.CELL_TYPE_NUMERIC:
+                        // Print cell value
+                        System.out.println(cell.getNumericCellValue());
+                        break;
+
+                    case Cell.CELL_TYPE_STRING:
+                        System.out.println(cell.getStringCellValue());
+                        break;
+                }
+                System.out.println("\t");
+
+            }
+
+        }
+    }
 
 
 }
